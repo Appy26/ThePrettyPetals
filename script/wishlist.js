e8187bcaab9e6e23a65660debfe53ee5c480584e
shireesha93470 <112804232+shireesha93470@users.noreply.github.com>
2023-01-23 11:01:49 +0530

    work completed

--- script/wishlist.js
+++ script/wishlist.js
@@ -1,12 +1,12 @@
-// navigation bar
-const CheckingIfLogIn = document.getElementById("nav-third-div");
+const cont = document.getElementById("nav-third-div");
 // Checking if signedIn
-
+signedIn()
 function signedIn() {
     let signedIn = localStorage.getItem("signedIn");
+    let admin_signedin = localStorage.getItem("admin-signed");
 
     if(signedIn == "true"){
-        CheckingIfLogIn.innerHTML = null;
+        cont.innerHTML = null;
         let div1 =document.createElement("div");
         let div2 = document.createElement("div");
         let div3 = document.createElement("div");
@@ -20,130 +20,58 @@ function signedIn() {
         img2.src = "./Photos/user.png";
         img3.src = "./Photos/shopping-cart.png";
         img4.src = "./Photos/wishlist.png";
+        div2.setAttribute("onclick", "location.href=`./userDashboard.html`");
+        div3.setAttribute("onclick", "location.href=`./cartPage.html`");
+        div4.setAttribute("onclick", "location.href=`./wishlist.html`");
 
         div1.append(img1);
         div2.append(img2);
         div3.append(img3);
         div4.append(img4);
-        CheckingIfLogIn.append(div1,div2,div3,div4);
+        cont.append(div1,div2,div3,div4);
         
     } else {
-        CheckingIfLogIn.innerHTML = null;
-        let btn1 = document.createElement("button");
-        btn1.setAttribute("id", "loginBtn");
-        btn1.setAttribute("onclick", "location.href=`./loginPage.html`");
+        if(admin_signedin == "true"){
+            cont.innerHTML = null;
+            let div2 = document.createElement("div");
 
-        let btn2 = document.createElement("button");
-        btn2.setAttribute("id", "registerBtn");
-        btn2.setAttribute("onclick", "location.href=`./registerPage.html`");
+            let img2 = document.createElement("img");
 
-        btn1.innerText = "Login";
-        btn2.innerText = "Register";
+            img2.src = "./Photos/user.png";
 
-        CheckingIfLogIn.style.marginRight = "7%"
+            div2.setAttribute("onclick", "location.href=`./admin.html`");
 
-        CheckingIfLogIn.append(btn1,btn2);
+            div2.append(img2);
+            cont.append(div2);
+        } else{
+            cont.innerHTML = null;
+            let btn1 = document.createElement("button");
+            btn1.setAttribute("id", "loginBtn");
+            btn1.setAttribute("onclick", "location.href=`./loginPage.html`");
+
+            let btn2 = document.createElement("button");
+            btn2.setAttribute("id", "registerBtn");
+            btn2.setAttribute("onclick", "location.href=`./registerPage.html`");
+
+            btn1.innerText = "Login";
+            btn2.innerText = "Register";
+
+            cont.style.marginRight = "7%"
+
+            cont.append(btn1,btn2);
+        }
+        
     }
 }
+
+
 // Search input part
+Search();
 function Search() {
     let search = document.getElementById("inp");
     let btn = document.getElementById("searchBtn");
 
     btn.addEventListener("click",()=>{
         localStorage.setItem("search",JSON.stringify(search.value));
-        btn.onclick="location.href=`./searchPage.html`";
     })
-}
-
-
-var user = JSON.parse(localStorage.getItem("user"));
-const url = `https://jsson-testing.onrender.com/`;
-var container = document.getElementById("card-items");
-var Data;
-var fullData;
-fetchData();
-async function fetchData() {
-    let req = await fetch(`${url}register/${user.id}/`);
-    let res = await req.json();
-    fullData = res;
-    Data = res.wishlist;
-    createDOM(Data);
-}
-
-
-function createDOM(data) {
-    if(!data){
-        let h3 = document.createElement("h3");
-        h3.innerText = "No Items to show here";
-        h3.style.textAlign = "center";
-        container.append(h3);
-        return;
-    }
-    container.innerHTML = null;
-    console.log(data);
-    data.forEach((el,i) => {
-        let card = document.createElement("div");
-        card.setAttribute("class","card");
-        let img = document.createElement("img");
-        let name = document.createElement("h3");
-        let price = document.createElement("p");
-        let imgDiv = document.createElement("div");
-        let nameDiv = document.createElement("div");
-        let remove = document.createElement("div");
-        remove.setAttribute("class","remove");
-        let p = document.createElement("p");
-        let add = document.createElement("div");
-        let p2 = document.createElement("p");
-        add.setAttribute("class","add-to-cart");
-        let div = document.createElement("div");
-        div.setAttribute("class","rem");
-
-        // Event Listeners
-        add.addEventListener("click",async()=>{
-            fullData.cart.push(el);
-            data.splice(i,1);
-            fullData.wishlist = data;
-            await fetch(`${url}register/${user.id}`,{
-                method:`PATCH`,
-                headers:{
-                    "content-type":"application/json"
-                },
-                body: JSON.stringify(fullData)
-            });
-            createDOM(data);
-        });
-        remove.addEventListener("click",async()=>{
-            data.splice(i,1);
-            fullData.wishlist = data;
-            await fetch(`${url}register/${user.id}`,{
-                method:`PATCH`,
-                headers:{
-                    "content-type":"application/json"
-                },
-                body: JSON.stringify(fullData)
-            });
-            createDOM(data);
-        });
-        
-        img.src = el.image;
-        name.innerText = el.name.substr(0,40);
-        price.innerText = `Price:- ${el.price}`;
-        p.innerText = "Remove Product";
-        p2.innerText = "Add To Cart";
-
-        add.append(p2);
-        remove.append(p);
-        div.append(add,remove);
-        imgDiv.append(img);
-        nameDiv.append(name,price,div);
-        card.append(imgDiv,nameDiv);
-        container.append(card);
-    });
-    if(data.length == 0){
-        let h3 = document.createElement("h3");
-        h3.innerText = "No Items to show here";
-        h3.style.textAlign = "center";
-        container.append(h3);
-    }
 }
